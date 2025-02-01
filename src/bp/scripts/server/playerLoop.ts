b5e143d8c6a2e71413b7ca80435cce4543dc0074
LuckedCoronet45 <lc45@lc-studios.net>
2025-02-01 11:35:58 +0900

    feat: add extraArgs to playerLoop callback function

--- src/bp/scripts/server/playerLoop.ts
+++ src/bp/scripts/server/playerLoop.ts
@@ -1,20 +1,26 @@
 import * as mc from "@minecraft/server";
 
-type PlayerLoopCallback = (player: mc.Player) => void;
+type PlayerLoopCallback = (player: mc.Player, extraArgs: PlayerLoopCallbackExtraArgs) => void;
 
-const EVENT_LISTENERS = new Set<PlayerLoopCallback>();
+type PlayerLoopCallbackExtraArgs = {
+	readonly healthComp: mc.EntityHealthComponent;
+	readonly equippableComp: mc.EntityEquippableComponent;
+	readonly mainhandSlot: mc.ContainerSlot;
+	readonly offhandSlot: mc.ContainerSlot;
+	readonly inventoryComp: mc.EntityInventoryComponent;
+	readonly inventoryContainer: mc.Container;
+};
 
-export function subscribe(callback: PlayerLoopCallback): PlayerLoopCallback {
-	if (EVENT_LISTENERS.has(callback)) throw new Error("The callback is already subscribed!");
+const LISTENERS = new Set<PlayerLoopCallback>();
+const CALLBACK_EXTRA_ARGS_CACHE = new Map<mc.Player, PlayerLoopCallbackExtraArgs>();
 
-	EVENT_LISTENERS.add(callback);
+mc.world.beforeEvents.playerLeave.subscribe(({ player }) => {
+	CALLBACK_EXTRA_ARGS_CACHE.delete(player);
+});
 
-	return callback;
-}
-
-export function unsubscribe(callback: PlayerLoopCallback): boolean {
-	return EVENT_LISTENERS.delete(callback);
-}
+mc.world.afterEvents.worldInitialize.subscribe(() => {
+	mc.system.runInterval(loop, 1);
+});
 
 function loop(): void {
 	const players = mc.world.getPlayers();
@@ -22,10 +28,44 @@ function loop(): void {
 	for (let i = 0; i < players.length; i++) {
 		const player = players[i]!;
 
-		for (const callback of EVENT_LISTENERS) {
-			callback(player);
+		for (const callback of LISTENERS) {
+			callback(player, getOrCreateCallbackExtraArgs(player));
 		}
 	}
 }
 
-mc.system.runInterval(loop, 1);
+function getOrCreateCallbackExtraArgs(player: mc.Player): PlayerLoopCallbackExtraArgs {
+	const cache = CALLBACK_EXTRA_ARGS_CACHE.get(player);
+
+	if (cache) return cache;
+
+	const healthComp = player.getComponent("health")!;
+	const inventoryComp = player.getComponent("inventory")!;
+	const equippableComp = player.getComponent("equippable")!;
+
+	const inventoryContainer = inventoryComp.container!;
+
+	const mainhandSlot = equippableComp.getEquipmentSlot(mc.EquipmentSlot.Mainhand);
+	const offhandSlot = equippableComp.getEquipmentSlot(mc.EquipmentSlot.Offhand);
+
+	const obj: PlayerLoopCallbackExtraArgs = {
+		healthComp,
+		inventoryComp,
+		inventoryContainer,
+		equippableComp,
+		mainhandSlot,
+		offhandSlot,
+	};
+
+	CALLBACK_EXTRA_ARGS_CACHE.set(player, obj);
+
+	return obj;
+}
+
+export function subscribe(callback: PlayerLoopCallback): void {
+	LISTENERS.add(callback);
+}
+
+export function unsubscribe(callback: PlayerLoopCallback): boolean {
+	return LISTENERS.delete(callback);
+}
