19512e87057f85f0fd9fa26095c1eb88d7c0e71a
LuckedCoronet45 <lc45@lc-studios.net>
2025-03-22 10:32:34 +0900

    build: remove old build script

--- scripts/types.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Represents either a successful result with a value or a failure with an error.
- */
-declare type Either<Value, Error = unknown> =
-	| { success: true; value: Value; error?: never }
-	| { success: false; error: Error; value?: never };
-
-declare type PackType = "RP" | "BP";
