0efc88cec2bd811bd98a21f0ef407ddc8f051478
LuckedCoronet45 <lc45@lc-studios.net>
2025-03-22 10:31:50 +0900

    build: add ganban build config files

---
+++ .ganbanrc.dev.mjs
@@ -0,0 +1,10 @@
+/** @type {import('ganban').Config} */
+export default {
+	bpSrc: "src/bp",
+	bpName: "SCPDY_BP",
+	rpSrc: "src/rp",
+	rpName: "SCPDY_RP",
+	tsconfig: "tsconfig.json",
+	sourcemap: true,
+	watch: true,
+};
---
+++ .ganbanrc.optimize.mjs
@@ -0,0 +1,11 @@
+/** @type {import('ganban').Config} */
+export default {
+	bpSrc: "src/bp",
+	bpName: "SCPDY_BP",
+	bpTarget: "dist",
+	rpSrc: "src/rp",
+	rpName: "SCPDY_RP",
+	rpTarget: "dist",
+	tsconfig: "tsconfig.json",
+	minify: true,
+};
