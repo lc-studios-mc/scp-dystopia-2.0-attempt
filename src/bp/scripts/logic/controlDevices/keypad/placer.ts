d1af4c2d3dab7fbb33b6a2923b83f56277a3082a
LuckedCoronet45 <lc45@lc-studios.net>
2025-04-07 11:14:29 +0900

    feat(keypad): implement logic

--- src/bp/scripts/logic/controlDevices/keypad/placer.ts
+++ src/bp/scripts/logic/controlDevices/keypad/placer.ts
@@ -1,4 +1,15 @@
 import * as mc from "@minecraft/server";
+import { ModalFormData } from "@minecraft/server-ui";
+import { addControlDeviceModeDropdownToForm, setControlDeviceMode } from "../utils";
+import {
+	attachNewKeypadEntityTo,
+	setKeypadHint,
+	setKeypadLoudIncorrectBuzzer,
+	setKeypadO5Clearance,
+	setKeypadPassword,
+} from "./keypad";
+
+const KEYPAD_PLACER_ITEM_TYPE_ID = "lc:scpdy_keypad_placer";
 
 mc.system.beforeEvents.startup.subscribe(({ itemComponentRegistry }) => {
 	itemComponentRegistry.registerCustomComponent("scpdy:keypad_placer", {
@@ -9,6 +20,7 @@ mc.system.beforeEvents.startup.subscribe(({ itemComponentRegistry }) => {
 function onUseOn(arg: mc.ItemComponentUseOnEvent): void {
 	const player = arg.source;
 	if (!(player instanceof mc.Player)) return;
+	if (arg.blockFace === mc.Direction.Down || arg.blockFace === mc.Direction.Up) return;
 
 	processPlacementAsync(player, arg)
 		.catch(() => {
@@ -20,4 +32,73 @@ async function processPlacementAsync(
 	player: mc.Player,
 	event: mc.ItemComponentUseOnEvent,
 ): Promise<void> {
+	const formData = new ModalFormData()
+		.title({ translate: "scpdy.form.keypad.placementOpts.title" })
+		.label({ translate: "scpdy.form.keypad.placementOpts.warning" })
+		.textField(
+			{ translate: "scpdy.form.keypad.placementOpts.passwordField.label" },
+			{ translate: "scpdy.form.keypad.placementOpts.passwordField.placeholder" },
+		)
+		.textField(
+			{ translate: "scpdy.form.keypad.placementOpts.hintField.label" },
+			{ translate: "scpdy.form.keypad.placementOpts.hintField.placeholder" },
+		)
+		.toggle(
+			{ translate: "scpdy.form.keypad.placementOpts.loudIncorrectBuzzerToggle.label" },
+			false,
+		)
+		.divider();
+
+	addControlDeviceModeDropdownToForm(formData);
+
+	formData
+		.toggle({ translate: "scpdy.form.keypad.placementOpts.O5ClearanceToggle.label" })
+		.submitButton({ translate: "scpdy.form.keypad.placementOpts.submitButton" });
+
+	const response = await formData.show(player);
+
+	if (response.canceled) return;
+	if (!response.formValues) return;
+
+	const password = String(response.formValues[0]).trim();
+	const hint = String(response.formValues[1]);
+	const loudIncorrectBuzzer = response.formValues[2] === true;
+	const controlDeviceMode = Number(response.formValues[3]);
+	const o5Clearance = response.formValues[4] === true;
+
+	if (password === "") {
+		player.playSound("note.bass");
+		player.sendMessage({ translate: "scpdy.msg.keypad.placement.passwordRequired" });
+		return;
+	}
+
+	if (password.length > 15) {
+		player.playSound("note.bass");
+		player.sendMessage({ translate: "scpdy.msg.keypad.placement.passwordTooLong" });
+		return;
+	}
+
+	if (player.getGameMode() !== mc.GameMode.creative) {
+		const equippable = player.getComponent("equippable");
+		if (!equippable) return;
+
+		const mainhandSlot = equippable.getEquipmentSlot(mc.EquipmentSlot.Mainhand);
+		if (!mainhandSlot.hasItem()) return;
+		if (mainhandSlot.typeId !== KEYPAD_PLACER_ITEM_TYPE_ID) return;
+
+		const itemAmount = mainhandSlot.amount;
+		if (itemAmount === 1) {
+			mainhandSlot.setItem();
+		} else {
+			mainhandSlot.amount = itemAmount - 1;
+		}
+	}
+
+	const keypad = attachNewKeypadEntityTo(event.block, event.blockFace, player);
+
+	setKeypadPassword(keypad, password);
+	setKeypadHint(keypad, hint);
+	setKeypadLoudIncorrectBuzzer(keypad, loudIncorrectBuzzer);
+	setControlDeviceMode(keypad, controlDeviceMode);
+	setKeypadO5Clearance(keypad, o5Clearance);
 }
