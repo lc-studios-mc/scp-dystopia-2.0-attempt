314c01e9f8cbbdb93b79fb24adcbaee01d88b8b1
LuckedCoronet45 <lc45@lc-studios.net>
2025-04-10 10:20:35 +0900

    feat(pwrgrid): add pwr_node_placer item component (not implemented)

--- src/bp/scripts/logic/pwrgrid/pwrNode/placer.ts
+++ src/bp/scripts/logic/pwrgrid/pwrNode/placer.ts
@@ -1,1 +1,10 @@
 import * as mc from "@minecraft/server";
+
+mc.system.beforeEvents.startup.subscribe(({ itemComponentRegistry }) => {
+	itemComponentRegistry.registerCustomComponent("scpdy:pwr_node_placer", {
+		onUseOn,
+	});
+});
+
+function onUseOn(arg: mc.ItemComponentUseOnEvent): void {
+}
