c5cf815264559153261670dfbd5a8dce40bfacc2
LuckedCoronet45 <lc45@lc-studios.net>
2025-04-10 10:33:48 +0900

    feat(pwrgrid): update pwrNode/index.ts

--- src/bp/scripts/logic/pwrgrid/pwrNode/index.ts
+++ src/bp/scripts/logic/pwrgrid/pwrNode/index.ts
@@ -1,1 +1,11 @@
 import * as mc from "@minecraft/server";
+
+export const PWR_NODE_ENTITY_TYPE_ID = "lc:scpdy_pwr_node";
+
+export function placePwrNode(
+	dimension: mc.Dimension,
+	location: mc.Vector3,
+	direction: mc.Direction,
+): void {
+	dimension.playSound("place.iron", location, { pitch: 0.8, volume: 1.05 });
+}
