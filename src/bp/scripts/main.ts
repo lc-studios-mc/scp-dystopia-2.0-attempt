e4461449d2867b303deb99514850ca55093c97f1
LuckedCoronet45 <lc45@lc-studios.net>
2025-05-10 10:21:12 +0900

    update main.ts

--- src/bp/scripts/main.ts
+++ src/bp/scripts/main.ts
@@ -1,2 +1,18 @@
-import "./logic";
+// Import legacy modules
+import "./logic/advancedItem";
+import "./logic/ammo";
+import "./logic/blastDoor";
+import "./logic/blocks";
+import "./logic/cctv";
+import "./logic/components";
+import "./logic/config";
+import "./logic/controlDevices";
+import "./logic/facilityNetwork";
+import "./logic/gore";
+import "./logic/humanMobs";
+import "./logic/scps";
+import "./logic/theUnknown";
+import "./logic/uncategorized";
+import "./logic/weapons";
+
 import "./features";
