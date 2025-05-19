8a48856b435c1476d04d822e09224d201adab719
LuckedCoronet45 <lc45@lc-studios.net>
2025-05-19 10:53:58 +0900

    make relay doors independent of activators

--- src/bp/scripts/features/doors/relay_door_component.ts
+++ src/bp/scripts/features/doors/relay_door_component.ts
@@ -1,11 +1,11 @@
 import { destroyBlock } from "@/utils/block";
 import { flattenCoordinates, unflattenToCoordinates } from "@/utils/math";
 import * as mc from "@minecraft/server";
-import { ActivatorBlockEvents } from "../machinery/activator_block/events";
+import { MachineryInputEvents } from "../machinery/input/events";
+import { getRelativeBlockAtDirection } from "@/utils/direction";
+import { isHoldingWrench } from "@/utils/wrench";
 
 interface RelayDoorComponentParams {
-	minStepIndex?: number;
-	maxStepIndex?: number;
 	openSound?: {
 		id: string;
 		volume?: number;
@@ -20,13 +20,16 @@ interface RelayDoorComponentParams {
 
 const STATE = {
 	isBottomPart: "lc:is_bottom_part",
-	powerLevel: "lc:power_level",
+	detectRedstone: "lc:detect_redstone",
+	powerLevelMajor: "lc:power_level_major",
+	powerLevelMinor: "lc:power_level_minor",
 	stepMajor: "lc:step_major",
 	stepMinor: "lc:step_minor",
 } as const;
 
 const DEFAULT_MIN_STEP_INDEX = 0;
 const DEFAULT_MAX_STEP_INDEX = 15;
+const POWER_LEVEL_DECREASE_INTERVAL = 12;
 
 mc.system.beforeEvents.startup.subscribe(({ blockComponentRegistry }) => {
 	blockComponentRegistry.registerCustomComponent("scpdy:relay_door", COMPONENT);
@@ -37,62 +40,56 @@ const COMPONENT: mc.BlockCustomComponent = {
 		if (!block.hasTag("lc:relay_door")) return;
 
 		const params = unknownParams as RelayDoorComponentParams;
-		const minStepIndex = params.minStepIndex ?? DEFAULT_MIN_STEP_INDEX;
-		const maxStepIndex = params.maxStepIndex ?? DEFAULT_MAX_STEP_INDEX;
+		const minStepIndex = DEFAULT_MIN_STEP_INDEX;
+		const maxStepIndex = DEFAULT_MAX_STEP_INDEX;
 
 		const isBottomPart = Boolean(block.permutation.getState(STATE.isBottomPart));
 		const otherPartBlock = isBottomPart ? block.above() : block.below();
-
 		if (!otherPartBlock || otherPartBlock.typeId !== block.typeId) return;
 
-		if (mc.system.currentTick % 2 !== 0) return; // Update only once per 2 ticks
+		// --- Power Level Logic ---
+		const { major: currentPowerLevelMajor, minor: currentPowerLevelMinor } = getPowerLevel(block.permutation);
+		const currentPowerLevel = flattenCoordinates(currentPowerLevelMajor, currentPowerLevelMinor, 3);
+		const detectRedstone = Boolean(block.permutation.getState(STATE.detectRedstone));
+		let newPowerLevel = getUpdatedPowerLevel(currentPowerLevel, detectRedstone, block);
 
-		const currentPowerLevel = Number(block.permutation.getState(STATE.powerLevel));
-		const nextPowerLevel = currentPowerLevel > 0 ? currentPowerLevel - 1 : 0;
-		const isPowered = currentPowerLevel > 0;
-
+		// --- Step Logic ---
 		const { major: currentStepMajor, minor: currentStepMinor } = getStep(block.permutation);
-
 		const currentStepIndex = flattenCoordinates(currentStepMajor, currentStepMinor);
+		const newStepIndex = getUpdatedStepIndex(currentStepIndex, currentPowerLevel, minStepIndex, maxStepIndex);
 
-		const newStepIndex = (() => {
-			if (isPowered && currentStepIndex < maxStepIndex) {
-				return currentStepIndex + 1;
-			}
-			if (!isPowered && currentStepIndex > minStepIndex) {
-				return currentStepIndex - 1;
-			}
-			return currentStepIndex;
-		})();
-
-		const newStep = unflattenToCoordinates(newStepIndex);
-
+		// --- Apply State Changes ---
+		const newPowerLevelUnflat = unflattenToCoordinates(newPowerLevel, 3);
+		const newStepUnflat = unflattenToCoordinates(newStepIndex, 4);
 		block.setPermutation(
 			block.permutation
-				.withState(STATE.stepMajor, newStep.major)
-				.withState(STATE.stepMinor, newStep.minor)
-				.withState(STATE.powerLevel, nextPowerLevel),
+				.withState(STATE.powerLevelMajor, newPowerLevelUnflat.major)
+				.withState(STATE.powerLevelMinor, newPowerLevelUnflat.minor)
+				.withState(STATE.stepMajor, newStepUnflat.major)
+				.withState(STATE.stepMinor, newStepUnflat.minor),
 		);
-
 		otherPartBlock.setPermutation(
-			otherPartBlock.permutation.withState(STATE.stepMajor, newStep.major).withState(STATE.stepMinor, newStep.minor),
+			otherPartBlock.permutation
+				.withState(STATE.stepMajor, newStepUnflat.major)
+				.withState(STATE.stepMinor, newStepUnflat.minor),
 		);
 
-		if (params.openSound && isPowered && newStepIndex === 1) {
+		// --- Play Sounds ---
+		if (mc.system.currentTick % 2 !== 0) return currentStepIndex; // Sounds are also updated only every 2 ticks
+		if (params.openSound && currentPowerLevel > 0 && newStepIndex === 1) {
 			dimension.playSound(params.openSound.id, block.location, {
 				volume: params.openSound.volume,
 				pitch: params.openSound.pitch,
 			});
 		}
-
-		if (params.closeSound && !isPowered && newStepIndex === 14) {
+		if (params.closeSound && currentPowerLevel <= 0 && newStepIndex === 14) {
 			dimension.playSound(params.closeSound.id, block.location, {
 				volume: params.closeSound.volume,
 				pitch: params.closeSound.pitch,
 			});
 		}
 	},
-	onPlace({ block, dimension }) {
+	onPlace({ block }) {
 		const isBottomPart = Boolean(block.permutation.getState(STATE.isBottomPart));
 
 		if (!isBottomPart) {
@@ -115,7 +112,7 @@ const COMPONENT: mc.BlockCustomComponent = {
 
 		blockAbove.setPermutation(upperPartPermutation);
 	},
-	onPlayerDestroy({ block, destroyedBlockPermutation, player }) {
+	onPlayerDestroy({ block, destroyedBlockPermutation }) {
 		const isBottomPart = Boolean(destroyedBlockPermutation.getState(STATE.isBottomPart));
 		const otherPartBlock = isBottomPart ? block.above() : block.below();
 
@@ -123,7 +120,75 @@ const COMPONENT: mc.BlockCustomComponent = {
 
 		destroyBlock(otherPartBlock);
 	},
+	onPlayerInteract({ player, block: blockUnchecked }) {
+		if (!player) return;
+
+		if (!isHoldingWrench(player)) {
+			player.onScreenDisplay.setActionBar({ translate: "scpdy.misc.text.wrenchIsRequired" });
+			return;
+		}
+
+		const block = blockUnchecked.hasTag("lc:door_top_part") ? blockUnchecked.below()! : blockUnchecked;
+
+		const detectRedstone = Boolean(block.permutation.getState(STATE.detectRedstone));
+
+		block.setPermutation(block.permutation.withState(STATE.detectRedstone, !detectRedstone));
+
+		player.playSound("random.click");
+
+		if (detectRedstone) {
+			player.onScreenDisplay.setActionBar({
+				translate: "scpdy.relayDoor.text.redstoneDetection.off",
+			});
+		} else {
+			player.onScreenDisplay.setActionBar({
+				translate: "scpdy.relayDoor.text.redstoneDetection.on",
+			});
+
+			if (player.addTag("scpdy_warned_activator_redstone_detection_lag")) {
+				mc.system.runTimeout(() => {
+					if (!player.isValid) return;
+
+					player.sendMessage({
+						translate: "scpdy.relayDoor.text.redstoneDetection.warn",
+					});
+
+					player.playSound("note.bass");
+				}, 15);
+			}
+		}
+	},
 };
+
+// --- Helper Functions for onTick ---
+function getUpdatedPowerLevel(currentPowerLevel: number, detectRedstone: boolean, block: mc.Block): number {
+	let newPowerLevel = currentPowerLevel;
+	if (currentPowerLevel > 0 && mc.system.currentTick % POWER_LEVEL_DECREASE_INTERVAL === 0) {
+		newPowerLevel--;
+	}
+	const isRedstoneDetected = detectRedstone && checkRedstonePower(block);
+	if (isRedstoneDetected && newPowerLevel === 0) {
+		newPowerLevel = 1;
+	}
+	return newPowerLevel;
+}
+
+function getUpdatedStepIndex(currentStepIndex: number, currentPowerLevel: number, minStepIndex: number, maxStepIndex: number): number {
+	if (mc.system.currentTick % 2 !== 0) return currentStepIndex; // Steps are updated only every 2 ticks
+	if (currentPowerLevel > 0 && currentStepIndex < maxStepIndex) {
+		return currentStepIndex + 1;
+	}
+	if (currentPowerLevel <= 0 && currentStepIndex > minStepIndex) {
+		return currentStepIndex - 1;
+	}
+	return currentStepIndex;
+}
+
+function getPowerLevel(permutation: mc.BlockPermutation): { major: number; minor: number } {
+	const major = Number(permutation.getState(STATE.powerLevelMajor));
+	const minor = Number(permutation.getState(STATE.powerLevelMinor));
+	return { major, minor };
+}
 
 function getStep(permutation: mc.BlockPermutation): { major: number; minor: number } {
 	const major = Number(permutation.getState(STATE.stepMajor));
@@ -131,14 +196,73 @@ function getStep(permutation: mc.BlockPermutation): { major: number; minor: numb
 	return { major, minor };
 }
 
-ActivatorBlockEvents.on("onTickPower", ({ block, powered }) => {
-	if (!powered) return;
+function checkRedstonePower(block: mc.Block): boolean {
+	if (Number(block.getRedstonePower()) > 0) return true;
+	if (Number(block.below(1)?.getRedstonePower()) > 0) return true;
+	if (Number(block.below(2)?.getRedstonePower()) > 0) return true;
+	return false;
+}
 
-	const above = block.above();
-	if (!above || !above.hasTag("lc:relay_door")) return;
+MachineryInputEvents.on("onActivate", (data) => {
+	if (data.mode !== "powerActivators") return;
 
-	const isBottomPart = Boolean(above.permutation.getState(STATE.isBottomPart));
-	if (!isBottomPart) return;
+	const block = data.block ?? data.dimension.getBlock(data.location);
+	if (!block) return;
 
-	above.setPermutation(above.permutation.withState(STATE.powerLevel, 2));
+	const blockBehind = getRelativeBlockAtDirection(block, data.pulseDirection ?? mc.Direction.North);
+	if (!blockBehind) return;
+
+	sendPowerToActivatorBlocks(blockBehind);
 });
+
+function sendPowerToActivatorBlocks(origin: mc.Block): void {
+	tryCrossFrom(origin);
+}
+
+function tryCrossFrom(block: mc.Block): void {
+	setPowerLevel(block);
+
+	const north = block.north();
+	if (north && setPowerLevel(north)) {
+		const north2 = north.north();
+		if (north2) setPowerLevel(north2);
+	}
+
+	const south = block.south();
+	if (south && setPowerLevel(south)) {
+		const south2 = south.south();
+		if (south2) setPowerLevel(south2);
+	}
+
+	const west = block.west();
+	if (west && setPowerLevel(west)) {
+		const west2 = west.west();
+		if (west2) setPowerLevel(west2);
+	}
+
+	const east = block.east();
+	if (east && setPowerLevel(east)) {
+		const east2 = east.east();
+		if (east2) setPowerLevel(east2);
+	}
+}
+
+function setPowerLevel(relayDoorBlock: mc.Block, powerLevel = 6): boolean {
+	if (!relayDoorBlock.isValid) return false;
+
+	if (!relayDoorBlock.hasTag("lc:relay_door")) return false;
+
+	if (relayDoorBlock.hasTag("lc:door_top_part")) {
+		return setPowerLevel(relayDoorBlock.below()!); // Top part does not simulate power levels
+	}
+
+	const powerLevelUnflat = unflattenToCoordinates(powerLevel, 3);
+
+	relayDoorBlock.setPermutation(
+		relayDoorBlock.permutation
+			.withState(STATE.powerLevelMajor, powerLevelUnflat.major)
+			.withState(STATE.powerLevelMinor, powerLevelUnflat.minor),
+	);
+
+	return true;
+}
