d198ffa7d322b5192f4059fc3ae21d8f26d7aaf1
LuckedCoronet45 <lc45@lc-studios.net>
2025-05-30 18:16:36 +0900

    update dashBlock.ts

--- src/bp/scripts/logic/blocks/minigameBlocks/dashBlock.ts
+++ src/bp/scripts/logic/blocks/minigameBlocks/dashBlock.ts
@@ -11,19 +11,36 @@ function onTick(arg: mc.BlockComponentTickEvent): void {
 
 function onStepOn(arg: mc.BlockComponentStepOnEvent): void {
 	if (!arg.entity) return;
-	if (!(arg.entity instanceof mc.Player)) return;
 
 	const ticksUntilActiveAgain = arg.block.permutation.getState("lc:ticks_until_active_again") as number;
 
 	if (ticksUntilActiveAgain > 0) return;
 
-	const dashDir = vec3.normalize(arg.entity.getVelocity());
+	let dashDir: mc.Vector3;
+	if (arg.entity instanceof mc.Player) {
+		const viewDir = arg.entity.getViewDirection();
+		const viewDirNorm = vec3.normalize({ ...viewDir, y: 0 });
+
+		const moveDir = arg.entity.inputInfo.getMovementVector();
+		const moveDirRotated = vec3.changeDir(
+			{
+				x: moveDir.x,
+				y: 0,
+				z: moveDir.y,
+			},
+			viewDirNorm,
+		);
+
+		dashDir = vec3.normalize(moveDirRotated);
+	} else {
+		dashDir = vec3.normalize(arg.entity.getVelocity());
+	}
 
 	if (vec3.length(dashDir) < 5) {
 		arg.entity.applyKnockback(
 			{
-				x: dashDir.x * 8,
-				z: dashDir.z * 8,
+				x: dashDir.x * 7,
+				z: dashDir.z * 7,
 			},
 			0.04,
 		);
