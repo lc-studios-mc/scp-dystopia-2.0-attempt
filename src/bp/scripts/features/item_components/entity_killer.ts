e3192d0ae3fcfa95d3e26baff3b3d2070cf6fce1
LuckedCoronet45 <lc45@lc-studios.net>
2025-07-17 14:30:50 +0900

    add some entity-related tools

---
+++ src/bp/scripts/features/item_components/entity_killer.ts
@@ -0,0 +1,21 @@
+import * as mc from "@minecraft/server";
+
+mc.system.beforeEvents.startup.subscribe((event) => {
+	event.itemComponentRegistry.registerCustomComponent("scpdy:entity_killer", {
+		onHitEntity(arg) {
+			const source = arg.attackingEntity;
+			if (!(source instanceof mc.Player)) return;
+
+			if (source.playerPermissionLevel < mc.PlayerPermissionLevel.Operator) {
+				source.onScreenDisplay.setActionBar({ translate: "scpdy.misc.only_op_may_use_this_item" });
+				return;
+			}
+
+			try {
+				arg.hitEntity.kill();
+			} catch (error) {
+				source.onScreenDisplay.setActionBar(`§c${error}`);
+			}
+		},
+	});
+});
--- src/bp/scripts/features/item_components/index.ts
+++ src/bp/scripts/features/item_components/index.ts
@@ -1,5 +1,5 @@
-import "./deal_max_damage_on_hit";
 import "./dont_get_durability_damage_on_hit";
+import "./entity_damager";
+import "./entity_killer";
+import "./entity_remover";
 import "./faction_changer";
-import "./kill_target_on_hit";
-import "./remove_target_on_hit";
