1e7cb16fabc0840283e7f0b0d91e570fb0d2b062
LuckedCoronet45 <lc45@lc-studios.net>
2025-08-26 13:43:09 +0900

    Update scp500_pill.ts

--- src/bp/scripts/core/item_components/scp500_pill.ts
+++ src/bp/scripts/core/item_components/scp500_pill.ts
@@ -3,8 +3,16 @@ import * as mc from "@minecraft/server";
 mc.system.beforeEvents.startup.subscribe(({ itemComponentRegistry }) => {
 	itemComponentRegistry.registerCustomComponent("scpdy:scp500_pill", {
 		onConsume(arg) {
-			// Remove all current effects
-			arg.source.getEffects().forEach((effect) => arg.source.removeEffect(effect.typeId));
+			// Remove most negative effects
+			arg.source.removeEffect("wither");
+			arg.source.removeEffect("poison");
+			arg.source.removeEffect("slowness");
+			arg.source.removeEffect("mining_fatigue");
+			arg.source.removeEffect("nausea");
+			arg.source.removeEffect("blindness");
+			arg.source.removeEffect("weakness");
+			arg.source.removeEffect("hunger");
+			arg.source.removeEffect("instant_damage");
 
 			arg.source.addEffect("regeneration", 30 * mc.TicksPerSecond, {
 				amplifier: 2,
